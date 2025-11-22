75956731bced7ec0c17f4fff910b3fd6c6c8577d
Alexandre Sage (ASE) <alexandre.sage@adsoftware.fr>
2025-11-22 18:44:01 +0100

    fix: pwd

--- src/commands/builtins/pwd.rs
+++ src/commands/builtins/pwd.rs
@@ -8,7 +8,7 @@ pub struct Pwd;
 impl Command for Pwd {
     fn execute(&self, _args: &[String]) -> Result<CommandResult, CommandError> {
         std::env::current_dir()
-            .map(|path| CommandResult::Message(path.to_string_lossy().to_string()))
+            .map(|path| CommandResult::Message(path.to_string_lossy().to_string() + "\n"))
             .map_err(|err| CommandError::Unknown(err.to_string()))
     }
 }
